7fd3f5b455272c8eb18380f3038fecde03aab40b
YourGi <youremail@gmail.com>
2026-03-23 19:18:35 +0200

    inventory header done

---
+++ Inventory Management System/Inventory.h
@@ -0,0 +1,57 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include<vector>
+#include<fstream>
+#include<sstream>
+#include"Product.h"
+using namespace std;
+class inventory
+{
+private:
+	 vector <Product> products;
+public:
+	void saveToFile(string IMS) {};
+	void loadToFile(string IMS) {};
+
+	// team A --------------------------------- team A
+
+	      //                   rana
+	void addproduct(){
+	
+	
+	}
+	      //                   jana
+	void updatequantity() {
+
+
+
+	}
+         //                    habiba
+	void removeproduct() {
+
+
+
+	}
+       //                      rawan 
+	void displayinventory() {
+
+
+
+	}
+	double computetotalvalue() {
+
+	}
+
+	// TEAM B ---------------------           ------------------
+	void lowstockalert(int threshold) {
+
+
+
+	}
+
+
+
+
+};
+
